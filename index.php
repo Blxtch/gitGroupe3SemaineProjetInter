e40e02287500c1e9c930b011e20b8fef0d21a123
sAkaD0 <132373139+sAkaD0@users.noreply.github.com>
2023-12-19 14:32:58 +0100

    Clean MVC

--- index.php
+++ index.php
@@ -3,8 +3,13 @@
 //require 'Modele.php';
 
 try {
+<<<<<<< HEAD
     //$logins = getLogin();
     require 'view\vueAccueil.php';
+=======
+    $logins = getLogin();
+    require 'vueLogin.php';
+>>>>>>> 26eb457 (Clean MVC)
 }
 catch (Exception $e) {
     $msgErreur = $e->getMessage();
