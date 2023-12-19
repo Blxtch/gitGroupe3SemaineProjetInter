be528b82aba98d68b15d3b1597872288a3d5148d
sAkaD0 <132373139+sAkaD0@users.noreply.github.com>
2023-12-19 14:34:41 +0100

    LoginVue

--- vueLogin.php
+++ vueLogin.php
@@ -1,2 +1,13 @@
 <?php require 'gabarit.php' ?>
 
+<h2>Login</h2>
+    <form action="login.php" method="post">
+        <label for="login">Nom d'utilisateur:</label>
+        <input type="text" id="login" name="login" required>
+        <br>
+        <label for="mdp">Mot de passe:</label>
+        <input type="password" id="mdp" name="mdp" required>
+        <br>
+        <input type="submit" value="Login">
+    </form>
+</div>
