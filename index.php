231c7c94cad26e5cdaf8355c7b6b3d4911c4d864
Blxtch <moreau.romain@hotmail.com>
2023-12-19 21:33:09 +0100

    Mise en poo + "routage..."

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
+require_once "Modele.php";
+$Modele_en_Session = new Modele;
+
+$db = $Modele_en_Session->getInstance();
 session_start();
+
 try {
     require 'vueLogin.php';
-    $login = getLogin();
+    $login = $Modele_en_Session->getLogin($login,$mdp);
 }
 catch (Exception $e) {
     $msgErreur = $e->getMessage();
