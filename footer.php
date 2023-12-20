d2be47808c64589d9cc5f255d112b2c17984f11c
Degueldre Ugo <132373139+sAkaD0@users.noreply.github.com>
2023-12-20 12:55:50 +0000

    DELETE -  remise en forme projet de base

--- footer.php
+++
@@ -1,3 +0,0 @@
-<footer id="piedPage">
-    <p>- Projet réalisé par le Groupe 3 HEH</p>
-</footer>
--- header.php
+++
@@ -1,6 +0,0 @@
-<header>
-                <img src="img/logo.png" alt="logo">
-                <?php
-                   // echo '<p>','Bonjour ', $users['prenom_user'],'</p>';
-                ?>         
-</header>
