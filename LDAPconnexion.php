69955f63aa6c6d737676283b2aff29f20458a229
sAkaD0 <132373139+sAkaD0@users.noreply.github.com>
2023-12-20 14:21:09 +0100

    LDAP fini

--- LDAPconnexion.php
+++ LDAPconnexion.php
@@ -1,31 +1,39 @@
 <?php
-
-$ldapServer = '192.168.110.1'; 
-$ldapPort = 389; 
-$ldapUser = 'julien.magnier@delivisim.lan'; 
-$ldapPassword = 'Test123*'; 
-
-
-$ldapConn = ldap_connect($ldapServer, $ldapPort) or die("Error");
-
-if ($ldapConn) {
-  
-    ldap_set_option($ldapConn, LDAP_OPT_PROTOCOL_VERSION, 3);
-
-   
-    $bind = ldap_bind($ldapConn, $ldapUser, $ldapPassword);
-
-    if ($bind) {
-        echo 'YOUHOUUUUUUUU';
-        ldap_close($ldapConn);
-    } else {
-      
-        echo 'Échec de l\'authentification LDAP';
+$ldap_connection = ldap_connect("delivisim.lan");
+$username = "julien.magnier@delivisim.lan"; 
+$password = "Test123*";
+$ldaprdn = 'PROJET' . "\\" . $username;
+echo $ldaprdn;
+echo $password;
+ldap_set_option($ldap_connection, LDAP_OPT_PROTOCOL_VERSION, 3);
+ldap_set_option($ldap_connection, LDAP_OPT_REFERRALS, 0);
+$bind = ldap_bind($ldap_connection, $ldaprdn, $password);
+if ($bind) {
+    $filter="(sAMAccountName=$username)";
+    $result = ldap_search($ldap_connection,"dc=projet,dc=lan",$filter);
+    $info = ldap_get_entries($ldap_connection, $result);
+    for ($i=0; $i<$info["count"]; $i++)
+    {
+        if($info['count'] > 1)
+            break;
+        echo "<p>You are accessing <strong> ". $info[$i]["sn"][0] .", " . $info[$i]["givenname"][0] ."</strong><br /> (" . $info[0]["dn"] .")</p>\n";
+        echo '<pre>';
+        
+        echo '</pre>';
+        $recup_dn = $info[$i]["distinguishedname"][0];
+        
+        $recup_dn = explode(",",$recup_dn);
+        
+        $recup_dn = explode("=",$recup_dn[1]);
+        $UO=$recup_dn[1];
+        echo $UO;
+        $userDn = $info[$i]["distinguishedname"][0]; 
     }
+    @ldap_close($ldap_connection);
 } else {
-    echo 'Échec de la connexion LDAP';
+    $msg = "Invalid email address / password";
+    echo $msg;
 }
-
-
+?>
 
 
